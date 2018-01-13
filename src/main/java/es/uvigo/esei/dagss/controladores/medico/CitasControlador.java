e4866670a2b621b34f34dc4a60456f163d4f0a77
Silvia Rodríguez <silviardguez@gmail.com>
2018-01-13 04:20:22 +0100

    Listar citas

---
+++ src/main/java/es/uvigo/esei/dagss/controladores/medico/CitasControlador.java
@@ -0,0 +1,118 @@
+package es.uvigo.esei.dagss.controladores.medico;
+
+import es.uvigo.esei.dagss.dominio.daos.CitaDAO;
+import es.uvigo.esei.dagss.dominio.daos.MedicoDAO;
+import es.uvigo.esei.dagss.dominio.daos.PacienteDAO;
+import es.uvigo.esei.dagss.dominio.entidades.Cita;
+import es.uvigo.esei.dagss.dominio.entidades.EstadoCita;
+import es.uvigo.esei.dagss.dominio.entidades.Medico;
+import es.uvigo.esei.dagss.dominio.entidades.Paciente;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author dagss
+ */
+@Named(value = "citasControlador")
+@SessionScoped
+public class CitasControlador implements Serializable {
+    
+    @Inject
+    MedicoControlador medicoControlador;
+    
+    @Inject
+    CitaDAO citaDAO;
+
+    @Inject
+    MedicoDAO medicoDAO;
+
+    @Inject
+    PacienteDAO pacienteDAO;
+
+    private List<Cita> citas;
+    private Cita citaActual;
+    private Medico medicoActual;
+
+    public CitasControlador() {
+    }
+
+    @PostConstruct
+    public void inicializar() {
+        medicoActual = medicoControlador.getMedicoActual();
+        citas = citaDAO.buscarPorMedico(medicoActual.getId());
+    }
+
+        /*
+       Usado en la lista desplegable con los estados de una cita
+     */
+    public EstadoCita[] getEstadosCitas() {
+        return EstadoCita.values();
+    }
+    
+    public List<Cita> getCitas() {
+        return citas;
+    }
+
+    public void setCitas(List<Cita> citas) {
+        this.citas = citas;
+    }
+
+    public Cita getCitaActual() {
+        return citaActual;
+    }
+
+    public void setCitaActual(Cita citaActual) {
+        this.citaActual = citaActual;
+    }
+
+    public List<Paciente> getListadoPacientes() {
+        return pacienteDAO.buscarTodos();
+    }
+
+    public List<Medico> getListadoMedicos() {
+        return medicoDAO.buscarTodos();
+    }
+
+    public void doEliminar(Cita cita) {
+        System.out.println(">>> llama a elimina con "+cita);
+        citaDAO.eliminar(cita);
+        citas = citaDAO.buscarTodos(); // Actualizar lista de centros
+    }
+
+    public void doNuevo() {
+        
+    }
+
+    public void doVer(Cita cita) {
+        citaActual = cita;   // Otra alternativa: volver a refrescarlos desde el DAO
+    }
+
+    public void doEditar(Cita cita) {
+        citaActual = cita;   // Otra alternativa: volver a refrescarlos desde el DAO
+    }
+
+    public void doGuardarNuevo() {
+        // Crea un nuevo centro de salud
+        citaActual = citaDAO.crear(citaActual);
+        // Actualiza lista de centros de salud a mostrar
+        citas = citaDAO.buscarTodos();
+
+    }
+
+    public void doGuardarEditado() {
+        // Actualiza un centro de salud
+        citaActual = citaDAO.actualizar(citaActual);
+        // Actualiza lista de centros de salud a mostrar
+        citas = citaDAO.buscarTodos();
+    }
+
+    public String doVolver() {
+        return "../index?faces-redirect=true";
+    }
+
+}
--- src/main/java/es/uvigo/esei/dagss/dominio/daos/CitaDAO.java
+++ src/main/java/es/uvigo/esei/dagss/dominio/daos/CitaDAO.java
@@ -5,13 +5,25 @@
 package es.uvigo.esei.dagss.dominio.daos;
 
 import es.uvigo.esei.dagss.dominio.entidades.Cita;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
 
 
 @Stateless
 @LocalBean
 public class CitaDAO  extends GenericoDAO<Cita>{    
-
+    
+    public List<Cita> buscarPorMedico(long id) {
+        Date fechaActual = Calendar.getInstance().getTime();
+        TypedQuery<Cita> q = em.createQuery("SELECT c FROM Cita AS c "
+                + "  WHERE (c.medico.id = :id) AND (c.fecha = :fechaActual)", Cita.class);
+        q.setParameter("id",id);
+        q.setParameter("fechaActual", fechaActual);
+        return q.getResultList();
+    }
     // Completar aqui
 }
